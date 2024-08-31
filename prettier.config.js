71f3adcefb23784b4641f46c182f136eddfcc2e2
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2024-08-31 08:38:00 +0100

    Setup Prettier, also with ESLint

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  endOfLine: "lf",
+  printWidth: 80,
+  semi: false,
+  trailingComma: "none"
+}
+
+// @todo starter::Review the formatting settings defined here
+export default config

6b9e134842cb67bb568aa2cd9fffb3b55f1d156e
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2024-08-31 07:39:17 +0100

    Setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+import path from "node:path"
+import url from "node:url"
+
+/* eslint-disable import/no-extraneous-dependencies */
+import { FlatCompat } from "@eslint/eslintrc"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier"
+/* eslint-enable import/no-extraneous-dependencies */
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+})
+
+export default tseslint.config(
+  // File ignore list
+  { ignores: ["node_modules/*", ".next/*"] },
+
+  // Extended configs
+  ...compat.extends("airbnb"),
+  ...compat.extends("next/core-web-vitals"),
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+
+  {
+    files: ["**/*.{js,ts,tsx}"],
+
+    // @todo starter::Review these rules to make sure they align with your guidelines
+    // Make sure to run eslint on the project after making changes
+    rules: {
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        { js: "never", ts: "never", tsx: "never" }
+      ],
+
+      "import/prefer-default-export": "off",
+      "no-underscore-dangle": "off",
+
+      "react/jsx-filename-extension": [
+        "error",
+        { extensions: [".jsx", ".tsx"] }
+      ]
+    }
+  },
+
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true
+      }
+    },
+
+    linterOptions: {
+      noInlineConfig: false,
+      reportUnusedDisableDirectives: true
+    },
+
+    settings: {
+      "import/resolver": {
+        "eslint-import-resolver-alias": {
+          map: [["@", "./src"]],
+          extensions: [".js", ".ts", ".tsx"]
+        }
+      }
+    }
+  }
+)

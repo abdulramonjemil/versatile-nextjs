d494b2e08acd2724c9d6444878e29e3857de9247
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2024-09-01 02:02:29 +0100

    Initialize shadcn UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss"
+// eslint-disable-next-line import/no-extraneous-dependencies
+import animate from "tailwindcss-animate"
 
 const config: Config = {
+  darkMode: ["class"],
   // @todo starter::Change this if you use different file conventions
   content: [
     "./src/app/**/*.{ts,tsx}",
@@ -15,41 +18,57 @@ const config: Config = {
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-
-        muted: "hsl(var(--muted))",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))"
+        },
         "muted-foreground": "hsl(var(--muted-foreground))",
-
-        card: "hsl(var(--card))",
-        "card-foreground": "hsl(var(--card-foreground))",
-
-        popover: "hsl(var(--popover))",
-        "popover-foreground": "hsl(var(--popover-foreground))",
-
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))"
+        },
         border: "hsl(var(--border))",
-
         input: "hsl(var(--input))",
         concentrated: "hsl(var(--concentrated))",
-
-        primary: "hsl(var(--primary))",
-        "primary-foreground": "hsl(var(--primary-foreground))",
-
-        secondary: "hsl(var(--secondary))",
-        "secondary-foreground": "hsl(var(--secondary-foreground))",
-
-        accent: "hsl(var(--accent))",
-        "accent-foreground": "hsl(var(--accent-foreground))",
-
-        destructive: "hsl(var(--destructive))",
-        "destructive-foreground": "hsl(var(--destructive-foreground))",
-
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))"
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))"
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
         ring: "hsl(var(--ring))",
-
-        radius: "hsl(var(--radius))"
+        radius: "hsl(var(--radius))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))"
+        }
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
       }
     }
   },
 
-  plugins: []
+  plugins: [animate]
 }
 
 export default config

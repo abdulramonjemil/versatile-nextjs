a775421247c9e9c77277270ce0ad574da7342bc1
Abdulramon Jemil <abdulramonjemil@gmail.com>
2024-09-25 11:25:36 +0100

    Use '.clientx' instead of '.component.tsx' for client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/client/theme.component"
-import { TRPCAppProvider } from "@/trpc/client.component"
+import { ThemeProvider } from "@/clientx/theme"
+import { TRPCAppProvider } from "@/trpc/clientx"
 
 import "./globals.css"
 

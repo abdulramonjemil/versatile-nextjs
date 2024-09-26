2c1c57f2e549bc52b478ab38b84961b6afa7dca3
Abdulramon Jemil <abdulramonjemil@gmail.com>
2024-09-26 21:50:18 +0100

    Reconfigure trpc exports

--- src/trpc/base.ts
+++ src/trpc/base.ts
@@ -3,7 +3,6 @@ import "server-only"
 import { initTRPC } from "@trpc/server"
 
 // Avoid exporting the entire t-object since it's not very descriptive.
-const t = initTRPC.create({})
+const trpc = initTRPC.create({})
 
-export const { router } = t
-export const baseProcedure = t.procedure
+export const { router, procedure: baseProcedure } = trpc

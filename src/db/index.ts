90f606360cc4b60aff31ed78f5806c7ecfb54941
Abdulramon Jemil <abdulramonjemil@gmail.com>
2024-09-26 17:07:53 +0100

    Exclude schema from drizzle init by default

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,9 @@ import "server-only"
 import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
 import { SERVER_ENV_NEON_DB_CONNECTION_STRING } from "@/env/server"
-import { schema } from "./schema"
 
+// Use the following when using `db.query.<table>` syntax
+// Schema can get very big so it is omitted by default
 const sql = neon(SERVER_ENV_NEON_DB_CONNECTION_STRING)
-export const db = drizzle(sql, { schema })
+// export const db = drizzle(sql, { schema })
+export const db = drizzle(sql)
